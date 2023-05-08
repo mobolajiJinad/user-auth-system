eae1a791cd76c111fc212d77adfbc45d2e5c065a
omobolaji <abdjinadquadri@gmail.com>
2023-05-08 11:39:04 +0100

    Fixed passport callback uri

--- config/passport.js
+++ config/passport.js
@@ -32,7 +32,8 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL:
+          "https://squady-user-auth-system.vercel.app/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
